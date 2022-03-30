c009bc323de52861d4a3f838107dc42d1b350d8c
Ehsan G <ehsan@say.company>
2022-03-30 11:55:46 -0400

    hardhat more setup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,42 +1,58 @@
-import * as dotenv from "dotenv";
-
-import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-ethers";
 import "hardhat-gas-reporter";
+import "dotenv/config";
 import "solidity-coverage";
+import "hardhat-deploy";
+import { HardhatUserConfig } from "hardhat/config";
 
-dotenv.config();
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "";
+const KOVAN_RPC_URL = process.env.KOVAN_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  defaultNetwork: "hardhat",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    hardhat: {
+      chainId: 31337,
+    },
+    localhost: {
+      chainId: 31337,
+    },
+    rinkeby: {
+      url: RINKEBY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 4,
+    },
+    kovan: {
+      url: KOVAN_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 42,
     },
   },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
+  solidity: "0.8.9",
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // the index 0 will be the deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+  },
+  mocha: {
+    timeout: 200000, // 200 seconds max for running tests
   },
 };
 
---
+++ helper-hardhat-config.ts
@@ -0,0 +1,34 @@
+export interface networkConfigItem {
+  ethUsdPriceFeed?: string;
+  blockConfirmations?: number;
+}
+
+export interface networkConfigInfo {
+  [key: string]: networkConfigItem;
+}
+
+export const networkConfig: networkConfigInfo = {
+  localhost: {},
+  hardhat: {},
+  // Price Feed Address, values can be obtained at https://docs.chain.link/docs/reference-contracts
+  // Default one is ETH/USD contract on Kovan
+  rinkeby: {
+    blockConfirmations: 3,
+  },
+};
+
+export const developmentChains = ["hardhat", "localhost"];
+export const proposalsFile = "proposals.json";
+
+// Governor Values
+export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
+export const QUORUM_PERCENTAGE = 50; // Require 50% of voters to pass
+export const MIN_DELAY = 3600; // 1 hour
+export const VOTING_PERIOD = 5; // Blocks 45818 = 1 week in live network
+export const VOTING_DELAY = 1; // 1 Block - proposal vote becomes active
+export const VOTING_THRESHOLD = 1; // 1 Block - proposal vote becomes active
+export const NEED_RATIO = "0.5";
+
+export const NEW_NEED_RATIO = "0.7";
+export const FUNC = "updateNeedRatio";
+export const PROPOSAL_DESCRIPTION = "Proposal #1 Changed Need_Ratio to 0.7!";
