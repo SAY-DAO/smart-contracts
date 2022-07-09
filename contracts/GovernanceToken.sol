845e4395c68339b64ff4b85544044136b8e70301
Ehsan G <ehsan@say.company>
2022-07-09 14:51:59 -0400

    changed to nft voting

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -1,37 +1,35 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/draft-ERC721Votes.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract GovernanceToken is ERC20Votes {
-    uint256 public maxSupply = 1000000000000000000;
+contract GovernanceToken is ERC721, ERC721Burnable, Ownable, EIP712, ERC721Votes {
+    using Counters for Counters.Counter;
 
-    // ERC20Permit: Initializes the EIP712 domain separator using the name parameter, and setting version to "1".
+    Counters.Counter private _tokenIdCounter;
+
     constructor()
-        ERC20("GovernanceToken", "gSAY")
-        ERC20Permit("GovernanceToken")
-    {
-        _mint(msg.sender, maxSupply);
+        ERC721("GovernanceToken", "gSAY")
+        EIP712("GovernanceToken", "1")
+    {}
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
     }
 
-    // The following functions are overrides required by Solidity. (OpenZeppelin)
-    // To update snapshots - how many people and token each block?
-    function _afterTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override(ERC20Votes) {
-        super._afterTokenTransfer(from, to, amount);
-    }
+    // The following functions are overrides required by Solidity.
 
-    function _mint(address to, uint256 amount) internal override(ERC20Votes) {
-        super._mint(to, amount);
-    }
-
-    function _burn(address account, uint256 amount)
+    function _afterTokenTransfer(address from, address to, uint256 tokenId)
         internal
-        override(ERC20Votes)
+        override(ERC721, ERC721Votes)
     {
-        super._burn(account, amount);
+        super._afterTokenTransfer(from, to, tokenId);
     }
 }
